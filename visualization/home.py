a17f56461dae0faecced08f99099d6c4929a2e37
Masum Billal <proafxin@users.noreply.github.com>
2024-10-12 17:25:03 +0600

    added state

--- visualization/home.py
+++ visualization/home.py
@@ -6,14 +6,17 @@ st.title("Interactive Visualization of Vehicle Crash Data")
 from visualization.components import vehicles_crash_data  # noqa: E402
 
 st.write("# Data Cleaning and Initial Analysis")
+data_load_state = st.text("Loading data...")
 data = vehicles_crash_data()
-st.write("## Sample Data")
+data_load_state.text("Loading data...done!")
+
+st.subheader("Sample data")
 st.dataframe(data.head())
 
 st.write(f"#### Total data points: {data.shape[0]}")
 st.write(f"#### Number of variables: {data.shape[1]}")
 
-st.write("## Count by variables")
+st.subheader("Count by variables")
 count_columns = data.columns[2:3] + data.columns[7:]
 column = st.selectbox("Select a column", count_columns)
 st.dataframe(data[column].value_counts())
