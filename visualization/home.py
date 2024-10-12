ae8b1b798a1fb6cf924c19c83f2527bbdc42be1a
Masum Billal <proafxin@users.noreply.github.com>
2024-10-12 18:40:03 +0600

    added maps and widgets

--- visualization/home.py
+++ visualization/home.py
@@ -1,22 +1,25 @@
 import streamlit as st
 
 st.set_page_config(layout="wide")
-st.title("Interactive Visualization of Vehicle Crash Data")
+st.title("Interact and Visualize Vehicle Crash Data by MTA Open")
 
 from visualization.components import vehicles_crash_data  # noqa: E402
 
-st.write("# Data Cleaning and Initial Analysis")
+st.write("# Initial Analysis")
 data_load_state = st.text("Loading data...")
 data = vehicles_crash_data()
 data_load_state.text("Loading data...done!")
 
-st.subheader("Sample data")
-st.dataframe(data.head())
+if st.checkbox("Show raw data"):
+    st.subheader("Sample data")
+    st.dataframe(data.head())
 
 st.write(f"#### Total data points: {data.shape[0]}")
 st.write(f"#### Number of variables: {data.shape[1]}")
 
 st.subheader("Count by variables")
-count_columns = data.columns[2:3] + data.columns[7:]
-column = st.selectbox("Select a column", count_columns)
+count_columns = data.columns[2:3] + data.columns[7:-6] + data.columns[-5:]
+column = st.selectbox("Select a variable", count_columns)
+count_load_state = st.text(f"Count by {column}...")
 st.dataframe(data[column].value_counts())
+count_load_state.text(f"Count by `{column}` loaded.")
