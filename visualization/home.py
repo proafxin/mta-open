d30a23480a48530cdfcd1a9960d3aadced315d57
Masum Billal <proafxin@users.noreply.github.com>
2024-10-12 16:00:43 +0600

    added streamlit selectboxes

--- visualization/home.py
+++ visualization/home.py
@@ -1,12 +1,46 @@
+import polars as pl
 import streamlit as st
 
-from visualization.components import vehicles_data
+st.set_page_config(layout="wide")
 
-st.write(f"## Number of incidents: {vehicles_data.shape[0]}")
+from visualization.components import vehicles_data  # noqa: E402
 
+event_type = "Event_Type"
+
+st.write("# Interactive Visualization of Vehicle Crash Data")
+
+st.write("## Sample Data")
+st.dataframe(vehicles_data.head())
+
+st.write(f"### Total data points: {vehicles_data.shape[0]}")
+
+drop_columns = [
+    "Contributing_Factor_2_Description",
+    "Contributing_Factor_1_Description",
+    "Partial_VIN",
+    "Case_Vehicle_ID",
+]
+vehicles_data = vehicles_data.drop(drop_columns)
+
+vehicles_data = vehicles_data.filter(pl.col(event_type).is_not_null())
+collision = vehicles_data.filter(pl.col(event_type).str.contains("Collision"))
 
 st.write("## Count by columns")
-option = st.selectbox("#### Select a column", vehicles_data.columns)
+column = st.selectbox("Select a column", vehicles_data.columns)
+st.bar_chart(data=vehicles_data[column].value_counts(), x=column, y="count", color=column)
 
-if option:
-    st.bar_chart(data=vehicles_data[option].value_counts(), x=option, y="count")
+st.write("### Event counts")
+st.dataframe(vehicles_data[event_type].value_counts())
+
+crashes = vehicles_data.filter(~(pl.col(event_type).str.contains("Not")))
+crashes = crashes.filter(~(pl.col(event_type).str.contains("Non")))
+crashes = crashes.filter(~(pl.col(event_type).str.contains("Unknown")))
+st.write(f"## Total crash incidents: {crashes.shape[0]}")
+st.write("## Sample crash events.")
+st.dataframe(crashes.head())
+
+st.write("## Crashes by event type")
+event_selection = st.selectbox(label="Select an event type", options=crashes[event_type].unique().to_list())
+
+if event_selection:
+    st.bar_chart(data=crashes[event_type].value_counts(), x=event_type, y="count", color=event_type)
