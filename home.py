e557397fed03f86e5e8be525483b39de9db79fef
Masum Billal <proafxin@users.noreply.github.com>
2024-10-12 23:50:00 +0600

    fixed caching

--- home.py
+++ home.py
@@ -1,34 +1,15 @@
-import re
-
 import streamlit as st
-from polars import DataFrame, read_parquet
+
+from component import vehicles_crash_data
 
 st.set_page_config(layout="wide")
 st.title("Interact and Visualize Vehicle Crash Data by MTA Open")
 
 
-def sanitize(text: str, lower: bool = False) -> str:
-    if lower:
-        text = text.lower()
-
-    text = re.sub(r"[-. ]", "_", text)
-    pattern = r"[^a-zA-Z0-9_]"
-
-    return re.sub(pattern=pattern, string=text, repl="")
-
-
-@st.cache_resource
-def vehicles_crash_data() -> DataFrame:
-    data = read_parquet("data/vehicle_crash.parquet")
-    data.columns = [sanitize(column, lower=True) for column in data.columns]
-
-    return data
-
-
 st.write("# Initial Analysis")
-data_load_state = st.text("Downloading data...")
+data_load_state = st.text("Loading data...")
 data = vehicles_crash_data()
-data_load_state.text("Downloading data...done!")
+data_load_state.text("Loading data...done!")
 
 if st.checkbox("Show raw data"):
     st.subheader("Sample data")
