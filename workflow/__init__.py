71f9418b6eeabb90b07d8cb9762251543394bd93
Masum Billal <proafxin@users.noreply.github.com>
2024-10-13 10:17:30 +0600

    fixing deploy

--- workflow/__init__.py
+++
@@ -1,9 +0,0 @@
-# fmt: off
-from dagster import Definitions, load_assets_from_modules
-
-from workflow.assets import crashes, metrics
-
-crash_assets = load_assets_from_modules([crashes])
-metric_assets = load_assets_from_modules([metrics])
-
-defs = Definitions(assets=[*crash_assets, *metric_assets])
