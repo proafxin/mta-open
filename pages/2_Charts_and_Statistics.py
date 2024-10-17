f5d2604990d0d52414ce718d3d44595fdd9327fb
Masum Billal <proafxin@users.noreply.github.com>
2024-10-17 14:02:54 +0600

    organized metrics

--- pages/2_Charts_and_Statistics.py
+++ pages/2_Charts_and_Statistics.py
@@ -43,6 +43,30 @@ st.title("Statistics and Charts")
 #             st.metric(label="Injured", value=filtered["number_of_persons_injured"].sum())
 
 
+st.header("Safety Statistics")
+safety_columns = ["year", "borough"]
+
+status_readable_names = {"number_of_persons_killed": "death", "number_of_persons_injured": "injury"}
+
+for safety_column in safety_columns:
+    safety_statuses = ["number_of_persons_killed", "number_of_persons_injured"]
+    safety_cols = st.columns((1,) * len(safety_statuses), gap="medium")
+
+    for i, safety_status in enumerate(safety_statuses):
+        with safety_cols[i]:
+            st.subheader(f"By {safety_column} and {status_readable_names[safety_status]}")
+
+            safety_data = (
+                data.select([safety_column, safety_status])
+                .drop_nulls()
+                .group_by(safety_column)
+                .agg(pl.col(safety_status).sum())
+                .sort(by=safety_column)
+            )
+            st.dataframe(safety_data)
+    ""
+
+
 COLUMN_VALUES = {
     "year": range(2012, 2025),
     "borough": ["BRONX", "QUEENS", "BROOKLYN", "MANHATTAN", "STATEN ISLAND"],
