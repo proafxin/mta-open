990221caf9d986d4580752695e7f541979779e3d
Masum Billal <proafxin@users.noreply.github.com>
2024-10-19 01:57:13 +0600

    updated map

--- pages/3_🔥_Heatmaps.py
+++ pages/3_🔥_Heatmaps.py
@@ -1,7 +1,10 @@
 from datetime import date
 
+import folium
+import folium.map
 import polars as pl
 import streamlit as st
+from streamlit_folium import st_folium
 
 st.set_page_config(layout="wide")
 
@@ -24,7 +27,33 @@ def load_map_data(col: str) -> pl.DataFrame:
 COLOUR_RANGE = [[217, 20, 122, 200], [235, 100, 33, 200], [14, 166, 204, 200], [131, 28, 161, 200], [100, 10, 225, 220]]
 COLORS = ["#0044ff", "#ffaa00", "#00ff00", "#ff7f0e", "#2ca02c"]
 
+ALL_COLORS = [
+    "gray",
+    "lightgray",
+    "cadetblue",
+    "lightred",
+    "darkpurple",
+    "purple",
+    "beige",
+    "orange",
+    "blue",
+    "darkgreen",
+    "darkred",
+    "darkblue",
+    "black",
+    "pink",
+    "white",
+    "lightblue",
+    "lightgreen",
+    "red",
+    "green",
+]
 boroughs = ["BRONX", "QUEENS", "BROOKLYN", "MANHATTAN", "STATEN ISLAND"]
+
+COLORS = ["darkred", "darkpurple", "blue", "darkblue", "purple"]
+# np.random.seed = 10
+# COLORS = np.random.choice(ALL_COLORS, size=len(boroughs))
+
 color_map = {borough: color for borough, color in zip(boroughs, COLORS)}
 
 
@@ -49,10 +78,30 @@ for col in selected_boroughs:
 
 with st.container():
     map_data = pl.concat(data)
+    map_data = pl.DataFrame(map_data)
     if map_data.shape[0] == 0:
         st.info(f"No data found for {selected_boroughs} on {time}")
     else:
-        st.map(data=map_data, latitude="latitude", longitude="longitude", color="color", height=750)
+        center = (map_data["latitude"].mean(), map_data["longitude"].mean())
+        fl_map = folium.Map(location=center, tiles="cartodbpositron", zoom_start=10)
+
+        for row in map_data.to_dicts():
+            folium.Marker(
+                location=row["coordinate"],
+                popup=folium.Popup(html=row["borough"], parse_html=True),
+                tooltip=f"({row["latitude"]}, {row["longitude"]})",
+                icon=folium.Icon(color=color_map[row["borough"]], icon="angle"),
+            ).add_to(fl_map)
+        # HeatMapWithTime(data=map_data.drop("coordinate", "date", "borough")).add_to(fl_map)
+
+        st_folium(fig=fl_map, use_container_width=True)
+
+        # leaf = leafmap.Map(center=center)
+        # st.write(map_data.head())
+        # leaf.add_basemap("OpenTopoMap")
+
+        # leaf.add_heatmap(data=map_data, latitude="latitude", longitude="longitude")
+        # leaf.to_streamlit()
 
 
 map_state.text(f"Loading map of crashes by {option} = {time}...DONE!")  # type: ignore [str-bytes-safe]
