93061836288d71933d9626d05d37b5c73056c290
Masum Billal <proafxin@users.noreply.github.com>
2024-10-22 02:37:22 +0600

    added extra pages

---
+++ pages/100_ℹ️_About_Me.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("About")
+
+st.markdown(
+    "I am Masum Billal. Professionally, I am a software developer (mainly Python stack) with a focus on backend, data engineering and machine learning. By hobby, I study mathematics. Sometimes I publish on math or data science in journals or open source projects in github."
+)
+st.markdown("* [My Github](https://github.com/proafxin/)")
+st.markdown("* [My LinkedIn](https://www.linkedin.com/in/billalmasum93/)")
